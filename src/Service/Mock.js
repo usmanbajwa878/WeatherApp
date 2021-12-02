3ca0f6d0fd7137218e477f8881b25b4559049b2a
usmanbajwa <usmanbajwa878@gmail.com>
2021-12-03 01:07:07 +0500

    fixed cities

--- src/Service/Mock.js
+++ src/Service/Mock.js
@@ -1,156 +1,127 @@
+
+
+
+
 export const CITIES = [
     {
         id:1,
-        name:'Randers',
-        lat:56.460449,
-        lng:10.036367
+        name:'Akershus',
+        lat:59.91348606,
+        lng:11.34723651
     },
     {
         id:2,
-        name:'Herning',
-        lat:56.140934,
-        lng:8.968116
+        name:'Astfold',
+        lat:59.43697797,
+        lng:10.66915727
     },
     {
         id:3,
-        name:'Aalborg',
-        lat:57.046707,
-        lng:9.935932
+        name:'AustAgder ',
+        lat:59.75724265,
+        lng:8.766000553
     },
     {   id:4,
-        name:'Copenhagen',
+        name:'Buskerud',
         lat:55.676098,
-        lng:12.568337
+        lng:10.19073686
     },
     {
         id:5,
-        name:'Holstebro',
-        lat:56.362564,
-        lng:8.621231
+        name:'Finnmark',
+        lat:60.82000207,
+        lng:23.24167151
     },
     {
         id:6,
-        name:'Aarhus',
-        lat:56.1572,
-        lng:10.2107
+        name:'Hedmark',
+        lat:60.82000207,
+        lng:11.06900156
     },
     {
         id:7,
-        name:'Odense',
-        lat:55.4004,
-        lng:10.3833
+        name:'Hordaland',
+        lat:60.63000316,
+        lng:6.441003463
     },
     {
         id:8,
-        name:'Esbjerg',
-        lat:55.4670,
-        lng:8.4500
+        name:'MoreOgRomsdal',
+        lat:62.54541872,
+        lng:6.388023233
     },
     {
         id:9,
-        name:'Vejle',
-        lat:55.7090,
-        lng:9.5350
+        name:'NordTrondelag',
+        lat:64.86801597,
+        lng:11.20530025
     },
     {
         id:10,
-        name:'Roskilde',
-        lat:55.6500,
-        lng:12.0833
+        name:'Nordland',
+        lat:68.23328859,
+        lng:14.56669714
     },
     {
         id:11,
-        name:'Viborg',
-        lat:56.4333,
-        lng:9.4000
+        name:'Oppland',
+        lat:60.80002138,
+        lng:10.7000081
     },
     {
         id:12,
-        name:'Svendborg',
-        lat:55.0704,
-        lng:10.6167
+        name:'Oslo',
+        lat:59.91669029,
+        lng:10.74997921
     },
     {
         id:13,
-        name:'Hillerd',
-        lat:55.9333,
-        lng:12.3167
+        name:'Rogaland',
+        lat:58.84541201,
+        lng:5.690029662
     },
     {
         id:14,
-        name:'Frederikshavn',
-        lat:57.4337,
-        lng:10.5333
+        name:'SognOgFjordane',
+        lat:61.18329612,
+        lng:6.849999471
     },
+
     {
         id:15,
-        name:'Soro',
-        lat:55.4330,
-        lng:11.5667
+        name:'SorTrondelag',
+        lat:63.41665753,
+        lng:10.41666622
     },
     {
         id:16,
-        name:'Frederiksberg',
-        lat:55.6785,
-        lng:12.5221
+        name:'Telemark',
+        lat:59.19998985,
+        lng:9.600023558
     },
     {
         id:17,
-        name:'Soborg',
-        lat:55.7302,
-        lng:12.5098
+        name:'Troms',
+        lat:69.24061729,
+        lng:18.00860592
     },
     {
         id:18,
-        name:'Hvidovre',
-        lat:55.6503,
-        lng:12.4758
+        name:'Vest Agder',
+        lat:58.16664207,
+        lng:8.000017862
     },
     {
         id:19,
-        name:'Rodovre',
-        lat:55.6827	,
-        lng:12.4644
+        name:'Vestfold',
+        lat:59.26400109,
+        lng:10.42100147
     },
-    {
-        id:20,
-        name:'Charlottenlund',
-        lat:55.7537,
-        lng:12.5918
-    },
-    {
-        id:21,
-        name:'Ringe',
-        lat:55.2451,
-        lng:10.4706
-    },
-    {
-        id:22,
-        name:'Varde',
-        lat:55.6216,
-        lng:8.4807
-    },
-    {
-        id:23,
-        name:'Ronde',
-        lat:56.3015,
-        lng:10.4751
-    },
-    {
-        id:24,
-        name:'Aabybro',
-        lat:57.1500,
-        lng:9.7500
-    },
-    {
-        id:25,
-        name:'Grindsted',
-        lat:55.7540,
-        lng:8.9123
-    }
 
 
 ]
+
+
 
 
 
